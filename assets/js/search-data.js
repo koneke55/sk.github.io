5d7dac4ae3b2d8382d318c5128f95963d3d172ff
koneke55 <139575233+koneke55@users.noreply.github.com>
2025-10-27 13:47:18 +0000

    Deploying to gh-pages from @ koneke55/sk.github.io@54163699b683792e7d3b3d45a62f90aa4e05d821 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,14 +25,14 @@ ninja.data = [{
           },
         },{id: "nav-repositories",
           title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "Explore my GitHub repositories and contributions.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/sk.github.io/repositories/";
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Download my CV to learn more about my experience, education, and achievements.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/sk.github.io/cv/";
