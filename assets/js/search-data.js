49f7685d200aafb24978b9ec8b3690f149583c0a
koneke55 <139575233+koneke55@users.noreply.github.com>
2025-11-06 15:20:44 +0000

    Deploying to gh-pages from @ koneke55/sk.github.io@05afb21dfafaabc51332b76ee5427383415310dc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -65,13 +65,6 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/sk.github.io/books/";
               },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/sk.github.io/blog/";
-              },
             },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
